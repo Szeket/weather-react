5ec06f784c1e4730c369ac80b6319b1a94608381
oszka <oszka@protonmail.com>
2022-03-24 12:25:56 +0000

    adding a footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <h1>Weather Search Engine</h1>
         <Weather />
+        <footer>
+          Coded by
+          <a href="https://github.com/Szeket/weather-react"> Szeket</a>
+        </footer>
       </header>
     </div>
   );
